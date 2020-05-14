1c8384ce17e7dde953ae6e6ef3fc0b8d27e2136a
kajal <tiwarikajal1306@gmail.com>
2020-05-14 11:04:44 +0530

    Store the doublet combination as well as percentage

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,25 +1,36 @@
 #!/bin/bash -x
-declare -A Dictionary
-
-heads=0
-tails=0
+declare -A DoubleDict
+declare -A PercentDouble
+hh=0
+ht=0
+th=0
+tt=0
 
 for ((count=0; count<20; count++))
 do
-	result=$((RANDOM % 2))
-	if [ $result -eq 1 ]
+	result=$((RANDOM % 4))
+	if [ $result -eq 0 ]
 	then
-		$((heads++))
-	else
-		$((tails++))
+		$((hh++))
+	elif [ $result -eq 1 ]
+	then
+		$((ht++))
+	elif [ $result -eq 2 ]
+	then
+		$((th++))
+	elif [ $result -eq 3 ]
+	then
+		$((tt++))
 	fi
 done
 
-Dictionary[Head]=$heads
-Dictionary[Tail]=$tails
+DoubleDict[HH]=$hh
+DoubleDict[TT]=$tt
+DoubleDict[HT]=$ht
+DoubleDict[TH]=$th
 
-for i in ${!Dictionary[@]}
+for i in ${!DoubleDict[@]}
 do
-	percentResult=$((${Dictionary[$i]} * 100 / 20))
-	echo $i" percentage: "$percentResult
+	PercentDouble[$i]=$((${DoubleDict[$i]} * 100 / 20))
 done
+echo ${PercentDouble[@]}
