812c355387131ec59f7dbe037b9f43b6754c7472
kajal <tiwarikajal1306@gmail.com>
2020-05-14 10:47:31 +0530

    Store the combination in dictionary and determine the percentage

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,9 +1,25 @@
 #!/bin/bash -x
+declare -A Dictionary
 
-result=$((RANDOM % 2))
-if [ $result -eq 1 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+heads=0
+tails=0
+
+for ((count=0; count<20; count++))
+do
+	result=$((RANDOM % 2))
+	if [ $result -eq 1 ]
+	then
+		$((heads++))
+	else
+		$((tails++))
+	fi
+done
+
+Dictionary[Head]=$heads
+Dictionary[Tail]=$tails
+
+for i in ${!Dictionary[@]}
+do
+	percentResult=$((${Dictionary[$i]} * 100 / 20))
+	echo $i" percentage: "$percentResult
+done
